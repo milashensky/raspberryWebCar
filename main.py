0134086c82bad039eab26ae9b813ebe67dbfc0d4
Alexandr Milashensky <milashensky@leadport.ninja>
2018-06-01 15:39:03 +0300

    added soft turns

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ import json
 import sys
 import os
 import time
+import math
 
 from flask import Flask, send_from_directory, request
 from flask import render_template
 from modules.AMSpi import AMSpi
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__)))
-app = Flask(__name__)
 amspi = AMSpi()
-# Set PINs for controlling shift register (GPIO numbering)
 amspi.set_74HC595_pins(21, 20, 16)
-# Set PINs for controlling all 4 motors (GPIO numbering)
 amspi.set_L293D_pins(5, 6, 13, 19)
+app = Flask(__name__)
 
 
 @app.route('/static/<path:path>')
@@ -31,24 +30,18 @@ def parse_request():
     response = request.data
     if response:
         data = json.loads(response.decode())
-        print(data)
-        direction = data.get('direction', '')
-        if 'up' in direction:
-            amspi.stop_dc_motors([1,2,3,4])
-            amspi.run_dc_motors([amspi.DC_Motor_1, amspi.DC_Motor_3, amspi.DC_Motor_2, amspi.DC_Motor_4])
-        if 'down' in direction:
-            amspi.stop_dc_motors([1,2,3,4])
-            amspi.run_dc_motors([amspi.DC_Motor_1, amspi.DC_Motor_2, amspi.DC_Motor_4, amspi.DC_Motor_3], clockwise=False)
-        if 'left' in direction:
-            # print('turn left')
-            amspi.stop_dc_motors([1,2,3,4])
-            amspi.run_dc_motors([2, 3])
-            amspi.run_dc_motors([1, 4], clockwise=False)
-        if 'right' in direction:
-            amspi.stop_dc_motors([1, 2, 3, 4])
-            amspi.run_dc_motors([2, 3], clockwise=False)
-            amspi.run_dc_motors([1, 4])
-        if direction.strip() == '':
-            amspi.stop_dc_motors([1,2,3,4])
+        # print(data)
+        left = data.get('left', 0)
+        right = data.get('right', 0)
+        speed = data.get('speed', 0)
+        leftCl = False
+        rightCl = False
+        if left < 0:
+            leftCl = True
+        if right < 0:
+            rightCl = True
+        amspi.stop_dc_motors([1,2,3,4])
+        amspi.run_dc_motors([2, 3], clockwise=rightCl, speed=round(right * speed * 100 / 3))
+        amspi.run_dc_motors([1, 4], clockwise=leftCl, speed=round(left * speed * 100 / 3))
+        time.sleep(0.5)
     return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
-
--- static/script.js
+++ static/script.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", function() {
-    var sendPosition = function (position) {
+    var sendPosition = function (data) {
+        // console.log(data);
         var xhr = new XMLHttpRequest();
         xhr.open("POST", '/control/', true);
         xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
@@ -9,23 +10,39 @@ window.addEventListener("load", function() {
         //         console.log('done!');
         //     }
         // }
-        xhr.send(JSON.stringify({ direction: position }));
+        xhr.send(JSON.stringify(data));
     }
-    var last = '';
+    var last_speed = last_ang = 0,
+        speedT = 0.3,
+        angT = 5;
+    var timerId = '';
     var dynamic = nipplejs.create({
-        zone: document.getElementById('controller'),
-        color: 'blue'
+            zone: document.getElementById('controller'),
+            color: 'blue',
     });
+    let getSpeed = (x)=>(x > 3? 3: Math.round(x*100)/100);
     dynamic.on('start end', function(evt, data) {
         if (evt.type == 'end') {
-            sendPosition(' ');
-            console.log('se', evt.type);
+            sendPosition({left: 0, right: 0, speed: 0});
+            // console.log('se', evt.type);
         }
     }).on('move', function(evt, data) {
         if (data && data.direction){
-            if(last != data.direction.angle){
-                console.log('move', data);
-                sendPosition(data.direction.angle)
+            // console.log('move', data);
+            ang = -1 * (Math.abs(data.angle.degree) - 180) / 2;
+            speed = getSpeed(data.force);
+            if(Math.abs(last_speed - speed) > speedT || Math.abs(last_ang - ang) > angT){
+                last_speed = speed;
+                last_ang = ang;
+                // console.log('speed: ' + speed + '; ang: '+ ang);
+                clearTimeout(timerId);
+                timerId = setTimeout(function() {
+                    sendPosition({
+                        left: Math.round(Math.sin(ang*3.14/180) * 100)/100,
+                        right: Math.round(Math.cos(ang*3.14/180) * 100)/100,
+                        speed: speed,
+                    })
+                }, 100);
             }
             last = data.direction.angle;
         }
